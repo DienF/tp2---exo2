2917938e060daad904e404422238afc30b520475
DienF <68037919+DienF@users.noreply.github.com>
2020-07-23 15:16:07 +1100

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def chute():
-    global goutte, i
-    goutte = randint(0, 4)
-    while i < 4:
-        led.plot(goutte, i)
-        basic.pause(500)
-        led.unplot(goutte, i)
-        i += 1
-
 def on_button_pressed_a():
     global truc
     if truc != 0:
@@ -26,11 +17,21 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 i = 0
 goutte = 0
 truc = 0
+score = 0
 truc = 2
 led.plot(truc, 4)
-chute()
 
 def on_forever():
-    if True:
-        pass
+    global goutte, i, score
+    goutte = randint(0, 4)
+    while i < 4:
+        led.plot(goutte, i)
+        basic.pause(500)
+        led.unplot(goutte, i)
+        i += 1
+    if goutte == truc:
+        score += 1
+    else:
+        basic.show_string("Score:")
+        basic.show_number(score)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,41 +1,40 @@
-function chute() {
-    
-    goutte = randint(0, 4)
-    while (i < 4) {
-        led.plot(goutte, i)
-        basic.pause(500)
-        led.unplot(goutte, i)
-        i += 1
-    }
-}
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     if (truc != 0) {
         led.unplot(truc, 4)
         truc += -1
         led.plot(truc, 4)
     }
-    
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.B, function () {
     if (truc != 4) {
         led.unplot(truc, 4)
         truc += 1
         led.plot(truc, 4)
     }
-    
 })
 let i = 0
 let goutte = 0
 let truc = 0
+let score = 0
 truc = 2
 led.plot(truc, 4)
-chute()
-basic.forever(function on_forever() {
-    if (true) {
-        
+basic.forever(function () {
+    goutte = randint(0, 4)
+    i = 0
+    while (i < 4) {
+        led.plot(goutte, i)
+        basic.pause(400)
+        led.unplot(goutte, i)
+        i += 1
     }
-    
+    if (goutte == truc) {
+        score += 1
+        music.ringTone(523)
+    } else {
+        music.ringTone(175)
+        basic.pause(1000)
+        basic.showString("Score:")
+        basic.showNumber(score)
+        basic.pause(1000)
+    }
 })
